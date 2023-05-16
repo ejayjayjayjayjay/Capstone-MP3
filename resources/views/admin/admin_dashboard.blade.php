3018a3c302be92a0e15c8103155f454eca63e64c
ejayjayjayjayjay <vibarjosell@gmail.com>
2023-05-16 22:52:32 +0800

    change logo in dashboard

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -33,7 +33,7 @@
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
-            <img src="{{asset('adminbackend/assets/images/logo-icon.png')}}" class="logo-icon" alt="logo icon">
+            <img src="{{asset('adminbackend/assets/images/UnitopLogo02.png')}}" class="logo-icon" alt="logo icon">
         </div>
         <div>
             <h4 class="logo-text">Unitop</h4>
