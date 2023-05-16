0456b693a6d7ee1b4462d724dceb45e4b1207581
ejayjayjayjayjay <vibarjosell@gmail.com>
2023-05-16 23:07:17 +0800

    add admin logout Option

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -11,4 +12,15 @@ class AdminController extends Controller
         return view('admin.index');
 
     }// End Method
+
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    } //End Method
 }
