39d076cbc0228c79aaafce311bc268e09c907b93
ejayjayjayjayjay <vibarjosell@gmail.com>
2023-05-17 22:08:50 +0800

    add dashboard to agent and add logout functionality

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AgentController extends Controller
 {
     public function AgentDashboard() {
-        return view('agent.agent_dashboard');
+        return view('agent.index');
     }//End Method
+
+    public function AgentLogin() {
+        return view('agent.agent_login');
+
+    } // End Method
+
+    public function AgentDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/agent/login');
+    } //End Method
 }
--- routes/web.php
+++ routes/web.php
@@ -48,9 +48,11 @@ Route::middleware(['auth','role:admin'])->group(function(){
 
 Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+    Route::get('/agent/logout', [AgentController::class, 'AgentDestroy'])->name('agent.logout');
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
+Route::get('/agent/login', [AgentController::class, 'AgentLogin']);
 
 
 
