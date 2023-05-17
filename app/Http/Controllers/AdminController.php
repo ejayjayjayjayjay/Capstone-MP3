e0c5ccda44c9b82848a461a83403c1bc428ab702
ejayjayjayjayjay <vibarjosell@gmail.com>
2023-05-17 20:46:10 +0800

    add AdminProfileStore method in admin controller for updating profile and changin image and also add route for that

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,4 +36,30 @@ class AdminController extends Controller
        $adminData = User::find($id);
        return view('admin.admin_profile_view', compact('adminData'));
     } //End Method
+
+    public function AdminProfileStore(Request $request)
+    {
+      $id = Auth::user()->id;
+      $data = User::find($id);
+      $data->name = $request->name;
+      $data->email = $request->email;
+      $data->phone = $request->phone;
+      $data->address = $request->address;
+      @unlink(public_path('upload/admin_images/'.$data->photo));
+
+      if ($request->file('photo')) {
+        $file = $request->file('photo');
+        $filename = date('YmdHi').$file->getClientOriginalName();
+        $file->move(public_path('upload/admin_images'),$filename);
+        $data['photo'] = $filename;
+    }
+        $data->save();
+
+/*     $notification = array(
+        'message' => 'Admin Profile Updated Successfully',
+        'alert-type' => 'success'
+    ); */
+
+    return redirect()->back();
+    } //End Method
 }
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -48,73 +48,81 @@
         <div class="col-lg-8">
             <div class="card">
                 <div class="card-body">
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0">User Name</h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="text" class="form-control" value="{{ $adminData->username }}" />
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0">Full Name</h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="text" class="form-control" value="{{ $adminData->name }}" />
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0">Email</h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="text" class="form-control" value="{{ $adminData->email }}" />
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0">Phone</h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="text" class="form-control" value="{{ $adminData->phone }}" />
-                        </div>
-                    </div>
 
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0">Address</h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="text" class="form-control" value="{{ $adminData->address }}" />
-                        </div>
-                    </div>
 
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0">Photo</h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="file" class="form-control" id="image"/>
-                        </div>
+            <form method="post" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data">
+                @csrf
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">User Name</h6>
                     </div>
-
-                    <div class="row mb-3">
-                        <div class="col-sm-3">
-                            <h6 class="mb-0"></h6>
-                        </div>
-                        <div class="col-sm-9 text-secondary">
-                            <img id="showImage" src="{{ (!empty($adminData->photo)) ? url('upload/admin_images/'.$adminData->photo):url('upload/no_image.jpg') }}" alt="Admin" style="width: 100px; height: 100px;">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-sm-3"></div>
-                        <div class="col-sm-9 text-secondary">
-                            <input type="button" class="btn btn-primary px-4" value="Save Changes" />
-                        </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="text" class="form-control" value="{{ $adminData->username }}" disabled />
                     </div>
                 </div>
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Full Name</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="text" name="name" class="form-control" value="{{ $adminData->name }}" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="email" name="email" class="form-control" value="{{ $adminData->email }}" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Phone</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="text" name="phone" class="form-control" value="{{ $adminData->phone }}" />
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Address</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="text" name="address" class="form-control" value="{{ $adminData->address }}" />
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Photo</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="file" name="photo" class="form-control" id="image"/>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0"></h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <img id="showImage" src="{{ (!empty($adminData->photo)) ? url('upload/admin_images/'.$adminData->photo):url('upload/no_image.jpg') }}" alt="Admin" style="width: 100px; height: 100px;">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
+                    </div>
+                </div>
+            </div>
+
+            </form>
+
+
             </div>
 
         </div>
