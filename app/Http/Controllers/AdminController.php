1321fc68634c6fe81ba92e1e10fd38f64acf772f
ejayjayjayjayjay <vibarjosell@gmail.com>
2023-05-17 18:57:22 +0800

    add profile in admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,5 +28,12 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/admin/login');
+    } //End Method
+
+    public function AdminProfile()
+    {
+       $id = Auth::user()->id;
+       $adminData = User::find($id);
+       return view('admin.admin_profile_view', compact('adminData'));
     } //End Method
 }
